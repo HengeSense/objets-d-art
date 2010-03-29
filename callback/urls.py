f2f5fc79012ed537815d85ec1a3ee22958912a2c
matthew.joseph.scott <matthew.joseph.scott@e71e58c3-d047-0410-bcd6-69ac0cbb6c69>
2010-03-29 20:39:55 +0000

    Refactoring

--- callback/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-urlpatterns = patterns('project.objetsdart.callbacks.views',
-    (r'^google-redirect/$', 'checkout_redirect'),
-    (r'^gc/$', 'google_checkout'),
-    (r'^ledger/', include('project.objetsdart.ledger.urls.py')),
-    (r'^ajax/$', 'ajax'),
-    (r'^notifications/$', 'notifcations'),
-    (r'^maintenance/$', 'maintenance'),
-)
--- client/views.py
+++
@@ -1,3 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-
-@user_passes_test(lambda u: u.has_perm('clients.is_client'))
